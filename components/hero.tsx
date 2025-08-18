9be27bf6b9aeeab2ec7d626a280142257d095748
SURESH M <sureshchoudhary222810@gmail.com>
2025-08-18 20:55:58 +0530

    Auth and Backend Setup

---
+++ components/hero.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl">
+            {" "}
+            Your AI Career Coach for
+            <br />
+            Professional Success
+          </h1>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
+            Advacnce your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
+          </p>
+        </div>
+        <div>
+          <Link href={"/dashboard"}>
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          <Link href={"/dashboard"}>
+            <Button size="lg" className="px-8" variant="outline">
+              Demo Video
+            </Button>
+          </Link>
+        </div>
+
+        <div>
+          <div>
+            <Image
+              src={"/Hero-image-dark.png"}
+              width={1280}
+              height={720}
+              alt="Hero Image"
+              className="rounded-lg shadow-2x1 border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
