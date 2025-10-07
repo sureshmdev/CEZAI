31ab7ec109ab2f89e545e3669dda1304d3d6e4c5
SURESH M <sureshchoudhary222810@gmail.com>
2025-10-07 16:50:58 +0530

    Fix inngest api route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,9 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/function";
+import { generateUserInsights } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights],
+  functions: [generateUserInsights],
 });
